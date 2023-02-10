8147ea1fc119657cddfbea0807e387b01b62756d
Streitenberger20 <62723319+Streitenberger20@users.noreply.github.com>
2023-02-10 16:37:29 -0300

    Add files via upload

---
+++ ProductoVendido.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaGestion
+{
+    internal class ProductoVendido
+    {
+        private long _id;
+        private int _idProducto;
+        private long _stock;
+        private long _idVenta;
+
+        public ProductoVendido(long id, int idProducto, long stock, long idVenta)
+        {
+            Id = id;
+            IdProducto = idProducto;
+            Stock = stock;
+            IdVenta = idVenta;
+        }
+
+        public long Id { get => _id; set => _id = value; }
+        public int IdProducto { get => _idProducto; set => _idProducto = value; }
+        public long Stock { get => _stock; set => _stock = value; }
+        public long IdVenta { get => _idVenta; set => _idVenta = value; }
+
+        public static List<ProductoVendido> traerProductosVendidos(long idusr)
+        {
+            List<ProductoVendido> productos = null;
+
+            string connectionString = "Data Source=DESKTOP-SPVRK7B;Initial Catalog=SistemaGestion;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+
+                SqlCommand command = new SqlCommand("SELECT productoVendido.id, productoVendido.stock, productoVendido.idProducto, productoVendido.idVenta FROM productoVendido INNER JOIN venta ON venta.idUsuario = @par AND venta.id = productoVendido.idVenta", connection);
+                var parametro = new SqlParameter();
+                parametro.ParameterName = "par";
+                parametro.SqlDbType = SqlDbType.BigInt;
+                parametro.Value = idusr;
+                command.Parameters.Add(parametro);
+
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    productos = new List<ProductoVendido>();
+
+                    while (reader.Read())
+                    {
+
+                        productos.Add(new ProductoVendido(reader.GetInt64(0), reader.GetInt32(1), reader.GetInt64(2), reader.GetInt64(3)));
+                    }
+                }
+
+            }
+            return productos;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace SistemaGestion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string connecionString = "Data Source=DESKTOP-SPVRK7B;Initial Catalog=SistemaGestion;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            //using (SqlConnection connection = new SqlConnection(connecionString))
+            //{
+            //    List<Usuario> usuarios = new List<Usuario>();
+
+            //    SqlCommand comando = new SqlCommand("SELECT * FROM Usuario", connection);
+            //    connection.Open();
+            //    SqlDataReader  reader = comando.ExecuteReader();
+
+            //    if (reader.HasRows)
+            //    {
+            //        while (reader.Read())
+            //        {
+            //            usuarios.Add(new Usuario(reader.GetInt64(0),reader.GetString(1), reader.GetString(2),reader.GetString(3), reader.GetString(4),reader.GetString(5)));
+
+
+            //        }
+
+            //        foreach (var item in usuarios){
+            //            Console.WriteLine(item.Nombre +" "+ item.Apellido);
+            //        }
+            //    }
+
+
+
+            //    connection.Close();
+
+            //} ;
+
+
+            List<ProductoVendido> productos = ProductoVendido.traerProductosVendidos(2);
+
+
+            Console.WriteLine(productos.Count);
+
+
+         }
+    }
+}
